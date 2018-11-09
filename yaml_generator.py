b89fdf32b2992fcad17eeace22a42af4f69c6516
jraysajulga <jraysajulga@gmail.com>
2018-11-08 23:36:34 -0600

    modularized and provides a trimmed galaxyp list

--- yaml_generator.py
+++ yaml_generator.py
@@ -2,29 +2,17 @@ import urllib2
 import ssl
 import json
 
-def create_yaml_and_tabular(filename, urls):
+def json_to_tabular(label, url):
+
+    # Loads JSON from the provided URL
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
-
-    data = []
-    for url in urls:
-        response = urllib2.urlopen(url,
+    response = urllib2.urlopen(url,
                     context=ctx)
-        #print(len(json.loads(response.read())))
-        data = data + json.loads(response.read())
+    data = json.loads(response.read())
 
-    yaml_file = open(filename + ".yaml", "w")
-
-    yaml_file.write("---\n"
-               "install_repository_dependencies: true\n"
-               "install_resolver_dependencies: true\n"
-               "install_tool_dependencies: false\n\n"
-               "tools:\n")
-
-    sheet_file = open(filename + ".tsv", "w")
-
-    # Extracts section, name, and owner into output table
+    # Arranges section, name, and owner from JSON into tabular output
     output = list()
     for category in data:
         if 'elems' in category:
@@ -34,13 +22,38 @@ def create_yaml_and_tabular(filename, urls):
                     owner = tool['tool_shed_repository']['owner']
                     section = tool['panel_section_name']
 
-                    output.append([section, name, owner])
+                    output.append([section, name, owner, label])
+                    
+    return output
 
+
+def remove_duplicates(duplicate_list):
     # Removes exact duplicates from the output list
     unique_list = list()
-    for interlist in output:
+    for interlist in duplicate_list:
         if interlist not in unique_list:
             unique_list.append(interlist)
+    return unique_list
+
+def remove_labeled_rows(all_tools, label):
+    output = []
+    for tool in all_tools:
+        if tool[3] != label:
+            output.append(tool)
+    return output
+
+def export_yaml_and_sheet(unique_list):
+    filename = "tools_galaxyp"
+    
+    yaml_file = open(filename + ".yaml", "w")
+
+    yaml_file.write("---\n"
+               "install_repository_dependencies: true\n"
+               "install_resolver_dependencies: true\n"
+               "install_tool_dependencies: false\n\n"
+               "tools:\n")
+
+    sheet_file = open(filename + ".tsv", "w")
 
     # Exports the data into a spreadsheet and galaxy YAML file
     for tool in unique_list:
@@ -52,5 +65,11 @@ def create_yaml_and_tabular(filename, urls):
     yaml_file.close()
     sheet_file.close()
 
-#create_yaml_and_tabular("tools_galaxyp", "https://galaxyp.msi.umn.edu/api/tools?in_panel=true")
-create_yaml_and_tabular("tools_galaxyp", ["https://proteomics.usegalaxy.eu/api/tools?in_panel=true", "https://galaxyp.msi.umn.edu/api/tools?in_panel=true"])
+
+proteomicsEU_tools = json_to_tabular("proteomicsEU", "https://proteomics.usegalaxy.eu/api/tools?in_panel=true")
+galaxyp_tools = json_to_tabular("galaxyp","https://galaxyp.msi.umn.edu/api/tools?in_panel=true")
+
+all_tools = remove_duplicates(proteomicsEU_tools + galaxyp_tools)
+trimmed_galaxyp_tools = remove_labeled_rows(all_tools, "proteomicsEU")
+
+export_yaml_and_sheet(trimmed_galaxyp_tools)
